0a3df75d3925ab5d22ad41a9365124ad98a7add6
MartinEskaros <martineskaros@gmail.com>
2023-09-09 16:37:14 -0400

    added functions to display numbers and clear screen when buttons are pressed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 function add(a,b){
     return a+b;
 }
@@ -14,9 +17,6 @@ function divide(a,b){
     return a/b;
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
 
 function operate(firstNumber,operator,secondNumber){
 
@@ -30,9 +30,9 @@ function operate(firstNumber,operator,secondNumber){
         return subtract(firstNumber,secondNumber);
         
 
-        case '*' : 
+        case 'x' : 
         return multiply(firstNumber,secondNumber);
-        break;
+        
 
         case '/' : 
         return divide(firstNumber,secondNumber);
@@ -42,3 +42,36 @@ function operate(firstNumber,operator,secondNumber){
     }
     
 }
+
+let currentInput="";
+let firstNumber =null;
+let operator=null;
+
+const screen = document.querySelector('.screen');
+
+const numberButtons = document.querySelectorAll('.numButton');
+numberButtons.forEach(button => {
+    button.addEventListener('click',function(e){
+        currentInput += e.target.innerText;
+        updateDisplay(currentInput);
+
+    });
+
+});
+
+function updateDisplay(value) {
+    screen.innerText = value;
+}
+
+function clearDisplay(){
+    const clearButton = document.getElementById('clear-button');
+    clearButton.addEventListener('click',() =>{
+        currentInput="";
+        firstNumber=null;
+        operator=null;
+        updateDisplay(currentInput);
+});
+}
+
+clearDisplay();
+
